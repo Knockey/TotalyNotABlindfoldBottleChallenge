6fa55ede39e885b5900c0b60105f1ae04cad31f1
Никита Байбородин <boratenot@gmail.com>
2021-10-15 15:26:23 +0800

    Joystick asset imported, test input system added.

---
+++ Assets/Scripts/JoystickPlayerExample.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class JoystickPlayerExample : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private FloatingJoystick _variableJoystick;
+    [SerializeField] private Rigidbody _rigidbody;
+
+    public void FixedUpdate()
+    {
+        Vector3 direction = Vector3.forward * _variableJoystick.Vertical + Vector3.right * _variableJoystick.Horizontal;
+
+        _rigidbody.AddForce(direction * _speed * Time.fixedDeltaTime, ForceMode.VelocityChange);
+    }
+}
