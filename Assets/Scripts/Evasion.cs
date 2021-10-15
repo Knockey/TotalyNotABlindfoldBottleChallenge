89e0a63ef6564a7f318d95a6daca47d1ed727a22
Никита Байбородин <boratenot@gmail.com>
2021-10-15 22:18:51 +0800

    First iteration of evasion done.

---
+++ Assets/Scripts/Evasion.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Evasion : MonoBehaviour
+{
+    [SerializeField] private Transform _center;
+    [SerializeField] private float _maxEvasionDistance;
+
+    public bool AbleToEvade(Vector3 nextPosition)
+    {
+        if (Vector3.Distance(nextPosition, _center.position) < _maxEvasionDistance)
+            return true;
+
+        return false;
+    }
+}
---
+++ Assets/Scripts/PlayerTestMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Evasion))]
+public class PlayerTestMovement : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private FloatingJoystick _variableJoystick;
+    [SerializeField] private Rigidbody _rigidbody;
+
+    private Evasion _evasion;
+
+    private void Awake()
+    {
+        _evasion = GetComponent<Evasion>();
+    }
+
+    public void FixedUpdate()
+    {
+        TryMove();
+    }
+
+    private void TryMove()
+    {
+        Vector3 direction = Vector3.forward * _variableJoystick.Vertical + Vector3.right * _variableJoystick.Horizontal;
+        Vector3 nextPosition = _speed * Time.fixedDeltaTime * direction + transform.position;
+
+        if (_evasion.AbleToEvade(nextPosition))
+            transform.position = Vector3.MoveTowards(transform.position, nextPosition, Time.fixedDeltaTime);
+    }
+}
